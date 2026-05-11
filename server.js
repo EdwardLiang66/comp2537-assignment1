90b516ad79de2112a5c88bf72798d620988844c7
Edward <jliang133@my.bcit.ca>
2026-05-11 10:37:01 -0700

    Fix Render login session saving

--- server.js
+++ server.js
@@ -58,9 +58,6 @@ app.use(
       dbName: MONGODB_DATABASE,
       collectionName: "sessions",
       ttl: 60 * 60,
-      crypto: {
-        secret: process.env.MONGODB_SESSION_SECRET,
-      },
     }),
     cookie: {
       maxAge: 1000 * 60 * 60,
